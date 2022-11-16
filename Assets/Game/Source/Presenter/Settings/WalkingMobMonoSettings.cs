21e81de3d29739b299bf154b5587095fe2869696
daniilbelov <tseflutty@yandex.ru>
2022-11-16 13:58:44 +0300

    Add walking mob mono settings

---
+++ Assets/Game/Source/Presenter/Settings/WalkingMobMonoSettings.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace Splatrika.MobArenaMobile.Presenter
+{
+    [RequireComponent(typeof(WalkingMobPresenter))]
+    public class WalkingMobMonoSettings : MonoBehaviour
+    {
+        public WalkingMobPresenter Presenter
+            => GetComponent<WalkingMobPresenter>();
+
+        public int Health => _health;
+        public float Speed => _speed;
+        public float WalkingRegenerationTime => _walkingRegenerationTime;
+        public float AtackDistance => _atackDistance;
+        public float AtackRegenerationTime => _atackRegenerationTime;
+        public int AtackDamage => _atackDamage;
+
+        [SerializeField]
+        private int _health;
+
+        [Header("Movement")]
+
+        [SerializeField]
+        private float _speed;
+
+        [SerializeField]
+        [InspectorName("Regeneration Time")]
+        private float _walkingRegenerationTime;
+
+        [Header("Atack")]
+
+        [SerializeField]
+        [InspectorName("Distance")]
+        private float _atackDistance;
+
+        [SerializeField]
+        [InspectorName("Regeneration Time")]
+        private float _atackRegenerationTime;
+
+        [SerializeField]
+        [InspectorName("Damage")]
+        private int _atackDamage;
+
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, _atackDistance);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position,
+                transform.position + Vector3.forward * _speed);
+        }
+    }
+}
