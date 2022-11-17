23c7955930497955983ae9af674ddff0512e3484
daniilbelov <tseflutty@yandex.ru>
2022-11-17 16:39:18 +0300

    Add bullet presenter

---
+++ Assets/Game/Source/Presenter/BulletPresenter.cs
@@ -0,0 +1,48 @@
+using Splatrika.MobArenaMobile.Model;
+using UnityEngine;
+
+namespace Splatrika.MobArenaMobile.Presenter
+{
+    public class BulletPresenter : MonoBehaviour
+    {
+        private Bullet _model;
+        private Transform _transform;
+
+
+        public void Init(Bullet model)
+        {
+            _model = model;
+            _transform = transform;
+
+            _model.Activated += OnActivated;
+            _model.Deactivated += OnDeactivated;
+
+            gameObject.SetActive(_model.Active);
+        }
+
+
+        private void OnDestroy()
+        {
+            _model.Activated -= OnActivated;
+            _model.Deactivated -= OnDeactivated;
+        }
+
+
+        private void Update()
+        {
+            _transform.position = _model.Position;
+        }
+
+
+        private void OnActivated()
+        {
+            gameObject.SetActive(true);
+        }
+
+
+        private void OnDeactivated()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
