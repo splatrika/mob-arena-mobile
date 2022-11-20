8b7e39802888be237c8fc2b5c41e463c3452691f
daniilbelov <tseflutty@yandex.ru>
2022-11-20 16:31:54 +0300

    Update shooting mob spawner configuration: add constructor

--- Assets/Game/Source/Model/Waves/SpawnPoints/ShootingMobSpawnerConfiguration.cs
+++ Assets/Game/Source/Model/Waves/SpawnPoints/ShootingMobSpawnerConfiguration.cs
@@ -9,5 +9,18 @@ namespace Splatrika.MobArenaMobile.Model
         public int KindId;
         public Vector3 SpawnPosition;
         public Vector3 ShootingPosition;
+
+
+        public ShootingMobSpawnerConfiguration(
+            float spawnTime,
+            int kindId,
+            Vector3 spawnPosition,
+            Vector3 shootingPosition)
+        {
+            SpawnTime = spawnTime;
+            KindId = kindId;
+            SpawnPosition = spawnPosition;
+            ShootingPosition = shootingPosition;
+        }
     }
 }
