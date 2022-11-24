6e2cdbc6e504e429d07ba1ee20797ba180c82098
daniilbelov <tseflutty@yandex.ru>
2022-11-24 16:48:20 +0300

    Add wave notification ui

---
+++ Assets/Game/Source/UI/WaveNotificationUI.cs
@@ -0,0 +1,38 @@
+using Splatrika.MobArenaMobile.Model;
+using UnityEngine;
+using Zenject;
+
+namespace Splatrika.MobArenaMobile.UI
+{
+    public class WaveNotificationUI : MonoBehaviour
+    {
+        [SerializeField]
+        private NotificationView _view;
+
+        [SerializeField]
+        private string _messageFormat = "Wave {0}";
+
+        private ILevel _level;
+
+
+        [Inject]
+        public void Init(ILevel level)
+        {
+            _level = level;
+
+            _level.WaveChanged += OnWaveChanged;
+        }
+
+
+        private void OnDestroy()
+        {
+            _level.WaveChanged -= OnWaveChanged;
+        }
+
+
+        private void OnWaveChanged(int wave)
+        {
+            _view.Show(string.Format(_messageFormat, wave + 1));
+        }
+    }
+}
