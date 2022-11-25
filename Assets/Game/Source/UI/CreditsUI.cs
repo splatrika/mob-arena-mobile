e239fc8938e52c56c6988187f4f4665e419cf891
daniilbelov <tseflutty@yandex.ru>
2022-11-25 18:50:20 +0300

    Add credits ui

---
+++ Assets/Game/Source/UI/CreditsUI.cs
@@ -0,0 +1,30 @@
+using Splatrika.MobArenaMobile.Model;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Splatrika.MobArenaMobile.UI
+{
+    public class CreditsUI : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _back;
+
+        private IScenesService _scenesService;
+
+
+        [Inject]
+        public void Init(IScenesService scenesService)
+        {
+            _scenesService = scenesService;
+
+            _back.onClick.AddListener(OnBackButtonClicked);
+        }
+
+
+        private void OnBackButtonClicked()
+        {
+            _scenesService.Load(Scenes.StartScreen);
+        }
+    }
+}
