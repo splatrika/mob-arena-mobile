3783aa980b5f289a507e5ac57e2af4e2831b9f83
daniilbelov <tseflutty@yandex.ru>
2022-12-06 10:03:24 +0300

    Update shooting mob model: implement shooting interface

--- Assets/Game/Source/Model/Mobs/ShootingMob.cs
+++ Assets/Game/Source/Model/Mobs/ShootingMob.cs
@@ -1,10 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace Splatrika.MobArenaMobile.Model
 {
-    public class ShootingMob : Mob<ShootingMobConfiguration>
+    public class ShootingMob : Mob<ShootingMobConfiguration>, IShooting
     {
+        public bool IsGunTaken
+            => ((ShootingStrategy)AttackingStrategy).IsGunTaken;
+
         private readonly IPlayerCharacter _playerCharacter;
+
+        public event Action GunTaken;
+        public event IShooting.ShotAction Shot;
+        public event Action GunHidden;
 
 
         public ShootingMob(
@@ -21,6 +29,10 @@ namespace Splatrika.MobArenaMobile.Model
                 logger)
         {
             _playerCharacter = playerCharacter;
+
+            ((ShootingStrategy)AttackingStrategy).GunTaken += OnGunTaken;
+            ((ShootingStrategy)AttackingStrategy).Shot += OnShot;
+            ((ShootingStrategy)AttackingStrategy).GunHidden += OnGunHidden;
         }
 
 
@@ -37,6 +49,32 @@ namespace Splatrika.MobArenaMobile.Model
                 .Setup(configuration.BulletsDamage, configuration.BulletsSpeed,
                 configuration.ShotPoint, gunTarget,
                 configuration.GunRegenerationTime);
+        }
+
+
+        protected override void OnDispose()
+        {
+            ((ShootingStrategy)AttackingStrategy).GunTaken -= OnGunTaken;
+            ((ShootingStrategy)AttackingStrategy).Shot -= OnShot;
+            ((ShootingStrategy)AttackingStrategy).GunHidden -= OnGunHidden;
+        }
+
+
+        private void OnGunTaken()
+        {
+            GunTaken?.Invoke();
+        }
+
+
+        private void OnShot(Vector3 direction)
+        {
+            Shot?.Invoke(direction);
+        }
+
+
+        private void OnGunHidden()
+        {
+            GunHidden?.Invoke();
         }
     }
 }
