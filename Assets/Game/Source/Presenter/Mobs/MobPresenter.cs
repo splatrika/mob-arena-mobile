9f83acf4f984f26b677d78bb1c7635450f9c9034
daniilbelov <tseflutty@yandex.ru>
2022-12-06 10:57:09 +0300

    Add mob presenter base

---
+++ Assets/Game/Source/Presenter/Mobs/MobPresenter.cs
@@ -0,0 +1,56 @@
+using System;
+using Splatrika.MobArenaMobile.Model;
+using UnityEngine;
+
+namespace Splatrika.MobArenaMobile.Presenter
+{
+    public abstract class MobPresenter<TMob, TConfiguration>
+        : MonoBehaviour, IPresenter
+        where TMob : Mob<TConfiguration>
+        where TConfiguration : MobConfiguration
+    {
+        protected TMob Model { get; private set; }
+        protected Animator Animator => _animator;
+
+        [SerializeField]
+        private WalkingPresenter _walkingPresenter;
+
+        [SerializeField]
+        private Animator _animator;
+
+        object IPresenter.Model => Model;
+
+        protected abstract void OnInit();
+        protected abstract void OnDispose();
+
+
+        public void Init(TMob mob)
+        {
+            if (Model != null)
+            {
+                throw new InvalidOperationException("This presenter is busy");
+            }
+            if (!mob.Active)
+            {
+                throw new InvalidOperationException("The mob is not active");
+            }
+            Model = mob;
+
+            Model.Deactivated += OnDestroy;
+            _walkingPresenter.Init(Model, _animator, transform);
+            OnInit();
+        }
+
+
+        private void OnDestroy()
+        {
+            if (Model != null)
+            {
+                Model.Deactivated -= OnDestroy;
+                _walkingPresenter.Dispose();
+                OnDispose();
+                Model = null;
+            }
+        }
+    }
+}
